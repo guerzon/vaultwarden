ee320f294e42e7f96ca4a594f8af5847d726c9b6
dza89 <20373984+dza89@users.noreply.github.com>
2023-08-06 21:40:36 +0200

    bump

--- generate-readme.sh
+++ generate-readme.sh
@@ -5,8 +5,8 @@ if [ ! -d "readme-generator-for-helm" ]; then
     git clone https://github.com/bitnami-labs/readme-generator-for-helm
 fi
 cd readme-generator-for-helm/
-lima nerdctl build -t readme-gen .
+docker build -t readme-gen .
 cd ..
 
 # Run the tool and mount the current project directory.
-lima nerdctl run --rm -v $(pwd):/mnt -w /mnt readme-gen readme-generator -v charts/vaultwarden/values.yaml -r charts/vaultwarden/README.md 
+docker run --rm -v $(pwd):/mnt -w /mnt readme-gen readme-generator -v charts/vaultwarden/values.yaml -r charts/vaultwarden/README.md 
